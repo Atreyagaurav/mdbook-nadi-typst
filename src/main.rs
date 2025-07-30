2e9f280ce54154d7de1c8641f942fb76996ed24b
Gaurav Atreya <allmanpride@gmail.com>
2025-07-30 18:42:27 -0400

    add custom modifications for function labels

--- src/main.rs
+++ src/main.rs
@@ -34,10 +34,10 @@ fn main() -> std::io::Result<()> {
 ]
 #show link: it => {
   if type(it.dest) != str {
-    underline(text(fill:green, it))
+    text(fill:green, it)
   }
   else {
-    underline(text(fill:blue, it))
+    text(fill:blue, it)
   }
 }
 #show ref: underline
@@ -138,7 +138,7 @@ fn write_chapter(
         level -= 1;
         contents = contents.lines().skip(1).collect::<Vec<&str>>().join("\n");
     }
-    write_markdown(writer, contents, level, chapter.path, number)?;
+    write_markdown(writer, contents, level, chapter.path, &chapter.name, number)?;
 
     for item in chapter.sub_items {
         write_bookitem(writer, item, level + 1)?;
@@ -160,6 +160,7 @@ fn write_markdown(
     md: String,
     chap_level: usize,
     chap_path: Option<PathBuf>,
+    chap_name: &str,
     number: bool,
 ) -> std::io::Result<()> {
     let mut options = Options::empty();
@@ -171,6 +172,7 @@ fn write_markdown(
     let mut table: Option<MdTable> = None;
     let mut list: Option<u64> = None;
     let mut in_code = false;
+    let mut in_head = false;
     for event in parser {
         match event {
             Event::Code(c) => {
@@ -186,6 +188,8 @@ fn write_markdown(
                         .map(|l| l.trim_start_matches('!'))
                         .collect::<Vec<&str>>()
                         .join("\n")
+                } else if in_head {
+                    maybe_label(chap_name, c)
                 } else {
                     escape_typst(c)
                 };
@@ -203,7 +207,13 @@ fn write_markdown(
             Event::End(TagEnd::Paragraph) => write!(writer, "\n\n")?,
             Event::Start(Tag::Strong) => write!(writer, "*")?,
             Event::End(TagEnd::Strong) => write!(writer, "*")?,
-            Event::Start(Tag::Link { dest_url, .. }) => write!(writer, "#link(\"{dest_url}\")[")?,
+            Event::Start(Tag::Link { dest_url, .. }) => {
+                if let Some(table) = &mut table {
+                    table.thiscell.push_str(&format_internal_link(dest_url));
+                } else {
+                    write!(writer, "{}", format_internal_link(dest_url))?
+                }
+            }
             Event::Start(Tag::CodeBlock(ck)) => {
                 match ck {
                     CodeBlockKind::Fenced(lang) => writeln!(writer, "\n``````{lang}")?,
@@ -211,7 +221,13 @@ fn write_markdown(
                 }
                 in_code = true;
             }
-            Event::End(TagEnd::Link) => write!(writer, "]")?,
+            Event::End(TagEnd::Link) => {
+                if let Some(table) = &mut table {
+                    table.thiscell.push_str("]");
+                } else {
+                    write!(writer, "]")?;
+                }
+            }
             Event::End(TagEnd::CodeBlock) => {
                 in_code = false;
                 writeln!(writer, "\n``````")?
@@ -252,8 +268,10 @@ fn write_markdown(
                 } else {
                     write!(writer, "\n*")?;
                 }
+                in_head = true;
             }
             Event::End(TagEnd::Heading(_)) => {
+                in_head = false;
                 if number {
                     writeln!(writer)?;
                 } else {
@@ -342,6 +360,34 @@ fn write_markdown(
     }
 
     Ok(())
+}
+
+fn maybe_label(chap_name: &str, text: pulldown_cmark::CowStr) -> String {
+    if let Some((pre, post)) = text.split_once(" { #") {
+        let label = post.trim().trim_end_matches('}').trim();
+        format!(
+            "{pre} <{}:{label}>",
+            chap_name.to_lowercase().replace(" ", "_")
+        )
+    } else {
+        escape_typst(text)
+    }
+}
+
+fn format_internal_link(link: pulldown_cmark::CowStr) -> String {
+    if link.contains(".md#") {
+        if let Some((file, func)) = link.split_once('#') {
+            let path = PathBuf::from(file);
+            let fname = path
+                .file_stem()
+                .unwrap_or_default()
+                .to_string_lossy()
+                .to_lowercase()
+                .replace(" ", "_");
+            return format!("#link(<{fname}:{func}>)[");
+        }
+    }
+    format!("#link(\"{link}\")[")
 }
 
 fn escape_typst(text: pulldown_cmark::CowStr) -> String {
